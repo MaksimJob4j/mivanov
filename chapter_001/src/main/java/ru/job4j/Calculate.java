b1fcc01db3e21f13332f5e212181270afc765daa
MaksimJob4j <iv-maxim@ya.ru>
2017-08-30 23:41:49 +0300

    JavaDoc. Code convention [#4087]
    JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+*Class Calculate решение задачи #144
+*@autor mivanov
+*@since 30.08.2017
+*/
+
+
 class Calculate{
 	public static void main (String[] args){
 		System.out.println("Hello world.");
---
+++ chapter_001/src/main/java/ru/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Maksim Ivanov (mailto:iv-maxim@ya.ru)
+* @version $Id$
+* @since 0.1
+*
+*/
+package ru.job4j
