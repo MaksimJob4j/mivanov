ff825339d93375c28e7cbadbe1977f0bd520cca6
MaksimJob4j <iv-maxim@ya.ru>
2017-09-02 02:39:32 +0300

    3.1. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,16 @@
+package ru.job4j.max;
+
+/**
+ *  Max.
+ */
+public class Max {
+    /**
+     *
+     * @param first first
+     * @param second second
+     * @return max
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    };
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * MaxTest.
+ */
+public class MaxTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void whenCompareOneAndTowThenTwo() {
+        Max maximum = new Max();
+        int result = maximum.max(1, 2);
+        int expected = 2;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenCompareTwoAndOneThenTwo() {
+        Max maximum = new Max();
+        int result = maximum.max(2, 1);
+        int expected = 2;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenCompareTwoAndTwoThenTwo() {
+        Max maximum = new Max();
+        int result = maximum.max(2, 2);
+        int expected = 2;
+        assertThat(result, is(expected));
+    }
+
+}
