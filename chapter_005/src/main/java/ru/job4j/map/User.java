02d082c03e2f07f5602f8ba10fc0980d66803667
MaksimJob4j <iv-maxim@ya.ru>
2017-10-23 11:27:51 +0300

    4. Перекрывать equals [#1004]
    4. Перекрывать equals [#1004]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -98,8 +98,22 @@ public class User {
         this.birthday = birthday;
     }
 
+//    @Override
+//    public int hashCode() {
+//        return name != null ? name.hashCode() : 0;
+//    }
+
     @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        return name != null ? name.equals(user.name) : user.name == null;
     }
 }
