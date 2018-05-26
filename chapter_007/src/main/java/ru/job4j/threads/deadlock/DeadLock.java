bca3b66109a72c5533fc3f28d64ef7d397243a4f
MaksimJob4j <iv-maxim@ya.ru>
2018-05-26 11:07:39 +0300

    Дополнительное задание Multithreading - DeadLock[#54345]. (ref)
    Программа, уходящая в deadlock.
    Вариант без sleep().

--- chapter_007/src/main/java/ru/job4j/threads/deadlock/DeadLock.java
+++ chapter_007/src/main/java/ru/job4j/threads/deadlock/DeadLock.java
@@ -1,38 +1,39 @@
 package ru.job4j.threads.deadlock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DeadLock {
-    Object object1 = new Object();
-    Object object2 = new Object();
-    Runnable runnable1 = new Runnable() {
-        @Override
-        public void run() {
-            synchronized (object1) {
-                System.out.println("first start");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object2) {
-                    System.out.println("first");
-                }
+    private ReentrantLock lock1 = new ReentrantLock();
+    private ReentrantLock lock2 = new ReentrantLock();
+
+    private final Object object1 = new Object();
+    private final Object object2 = new Object();
+    private Runnable runnable1 = () -> {
+        synchronized (object1) {
+            lock1.tryLock();
+            System.out.println("First get object1 and lock1");
+            while (!lock2.isLocked()) {
+                Thread.yield();
             }
+            System.out.println("First try get object2");
+            synchronized (object2) {
+                System.out.println("First get object2");
+            }
+            lock1.unlock();
         }
     };
-    Runnable runnable2 = new Runnable() {
-        @Override
-        public void run() {
-            synchronized (object2) {
-                System.out.println("second start");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object1) {
-                    System.out.println("second");
-                }
+    private Runnable runnable2 = () -> {
+        synchronized (object2) {
+            lock2.tryLock();
+            System.out.println("Second get object2 and lock2");
+            while (!lock1.isLocked()) {
+                Thread.yield();
             }
+            System.out.println("Second try get object1");
+            synchronized (object1) {
+                System.out.println("Second get object1");
+            }
+            lock2.unlock();
         }
     };
 
