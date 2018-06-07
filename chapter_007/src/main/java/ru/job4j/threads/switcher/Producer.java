faf8a7fcdfabb66d7a4ab9c5bd821807d57e78f2
MaksimJob4j <iv-maxim@ya.ru>
2018-06-07 14:08:14 +0300

    Дополнительное задание Multithreading - Свитчер [#50581][#54360] В-3
    fair-lock
    2 потока по-оереди добавляют в строку по 10 символов

--- chapter_007/src/main/java/ru/job4j/threads/switcher/Producer.java
+++ chapter_007/src/main/java/ru/job4j/threads/switcher/Producer.java
@@ -1,38 +1,42 @@
 package ru.job4j.threads.switcher;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
 public class Producer extends Thread {
     private final Buffer buffer;
     private final int value;
-    private final AtomicInteger lock;
+    private final ReentrantLock lock;
+    private final AtomicInteger flag;
 
-    public Producer(final Buffer buffer, final int value, AtomicInteger lock) {
+    public Producer(final Buffer buffer, final int value, AtomicInteger flag, ReentrantLock lock) {
         this.buffer = buffer;
         this.value = value;
+        this.flag = flag;
         this.lock = lock;
     }
 
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            if (this.lock.get() != this.value) {
+            lock.lock();
+            if (this.flag.get() != this.value) {
                 IntStream.range(0, 10).forEach(
                         value -> buffer.add(this.value)
                 );
-                this.lock.set(this.value);
-            } else {
-                    Thread.yield();
+                this.flag.set(this.value);
             }
+            lock.unlock();
         }
     }
 
     public static void main(String[] args) {
         final Buffer buffer = new Buffer();
         final AtomicInteger flag = new AtomicInteger(2);
-        Thread thread1 = new Producer(buffer, 1, flag);
-        Thread thread2 = new Producer(buffer, 2, flag);
+        final ReentrantLock lock = new ReentrantLock(true);
+        Thread thread1 = new Producer(buffer, 1, flag, lock);
+        Thread thread2 = new Producer(buffer, 2, flag, lock);
         thread1.start();
         thread2.start();
         try {
