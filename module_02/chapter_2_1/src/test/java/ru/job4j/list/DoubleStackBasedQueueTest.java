a116b0023daaa36a45fb15cc497e638bf9f3645a
MaximIvanov <mailto:iv-maxim@ya.ru>
2018-10-10 09:42:49 +0300

    5.3.3.1 Очередь на двух стеках [#160] REF Правка тестов

--- module_02/chapter_2_1/src/test/java/ru/job4j/list/DoubleStackBasedQueueTest.java
+++ module_02/chapter_2_1/src/test/java/ru/job4j/list/DoubleStackBasedQueueTest.java
@@ -8,24 +8,57 @@ import static org.junit.Assert.*;
 public class DoubleStackBasedQueueTest {
 
     @Test
-    public void doubleStackBasedQueueTest() {
+    public void whenNewStackPollNull() {
         DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
         assertNull(queue.poll());
+    }
+
+    @Test
+    public void whenPushNullThenPollNull() {
+        DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
         queue.push(null);
         assertNull(queue.poll());
+    }
+
+    @Test
+    public void whenPushOneThenPollOne() {
+        DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
         queue.push(1);
         assertThat(queue.poll(), is(1));
         assertNull(queue.poll());
+    }
+
+    @Test
+    public void whenPushOneTwoThenPollOneTwo() {
+        DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
+        queue.push(1);
+        queue.push(2);
+        assertThat(queue.poll(), is(1));
+        assertThat(queue.poll(), is(2));
+        assertNull(queue.poll());
+    }
+
+    @Test
+    public void whenPushOneTwoThreeThenPollOneTwoThree() {
+        DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
+        queue.push(1);
         queue.push(2);
         queue.push(3);
+        assertThat(queue.poll(), is(1));
         assertThat(queue.poll(), is(2));
-        queue.push(4);
-        queue.push(5);
         assertThat(queue.poll(), is(3));
-        assertThat(queue.poll(), is(4));
-        queue.push(6);
-        assertThat(queue.poll(), is(5));
-        assertThat(queue.poll(), is(6));
+        assertNull(queue.poll());
+    }
+
+    @Test
+    public void whenRandomPushAndPollThenCorrectReturn() {
+        DoubleStackBasedQueue<Integer> queue = new DoubleStackBasedQueue<>();
+        queue.push(1);
+        queue.push(2);
+        assertThat(queue.poll(), is(1));
+        queue.push(3);
+        assertThat(queue.poll(), is(2));
+        assertThat(queue.poll(), is(3));
         assertNull(queue.poll());
     }
 }
