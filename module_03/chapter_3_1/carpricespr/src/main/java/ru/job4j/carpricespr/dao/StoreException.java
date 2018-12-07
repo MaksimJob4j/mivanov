6b29c7a75a576797ea0cbc0c016d2d664c1fceea
MaximIvanov <mailto:iv-maxim@ya.ru>
2018-12-08 02:40:16 +0300

    CarPrice Удалил неиспользуемые классы при переходе на CrudRepository

--- module_03/chapter_3_1/carpricespr/src/main/java/ru/job4j/carpricespr/dao/StoreException.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.carpricespr.dao;
-
-public class StoreException extends Exception {
-    public StoreException(Exception e) {
-        super(e);
-    }
-}
--- module_03/chapter_3_1/carpricespr/src/test/java/ru/job4j/carpricespr/helper/Wrapper.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.carpricespr.helper;
-
-public interface Wrapper<T> {
-    T get();
-    void set(T value);
-    boolean isEmpty();
-}
