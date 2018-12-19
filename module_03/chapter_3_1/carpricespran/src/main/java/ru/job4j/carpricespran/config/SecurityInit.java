085f729264830b21e0fc6c901baf7b7a133f40e2
MaximIvanov <mailto:iv-maxim@ya.ru>
2018-12-20 01:40:47 +0300

    i# This is a combination of 3 commits.
    CarPrice. Spring полностью на аннотациях (без Spring Boot).
    
    CarPrice. Spring полностью на аннотациях (без Spring Boot).
    
    CarPrice. Spring полностью на аннотациях (без Spring Boot).

---
+++ module_03/chapter_3_1/carpricespran/src/main/java/ru/job4j/carpricespran/config/SecurityInit.java
@@ -0,0 +1,7 @@
+package ru.job4j.carpricespran.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ module_03/chapter_3_1/carpricespran/src/main/java/ru/job4j/carpricespran/config/SpringWebConfig.java
@@ -0,0 +1,40 @@
+package ru.job4j.carpricespran.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.job4j.carpricespran.controllers")
+public class SpringWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(5242880);
+        resolver.setDefaultEncoding("utf-8");
+        return resolver;
+    }
+}
