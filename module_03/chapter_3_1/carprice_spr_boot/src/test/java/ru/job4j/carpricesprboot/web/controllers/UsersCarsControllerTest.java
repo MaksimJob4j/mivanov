41f14bd09ddc162533d2009290b4133ffd535cc8
MaximIvanov <mailto:iv-maxim@ya.ru>
2019-03-23 23:02:04 +0300

    Переход на JDK 11

--- module_03/chapter_3_1/carprice_spr_boot/src/test/java/ru/job4j/carpricesprboot/web/controllers/UsersCarsControllerTest.java
+++ module_03/chapter_3_1/carprice_spr_boot/src/test/java/ru/job4j/carpricesprboot/web/controllers/UsersCarsControllerTest.java
@@ -3,7 +3,9 @@ package ru.job4j.carpricesprboot.web.controllers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -20,7 +22,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(UsersCarsController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//@WebMvcTest(UsersCarsController.class)
 public class UsersCarsControllerTest {
 
     @Autowired
