4c70e3e6e5aa22394ec7a70a5900eac6b6fa0fbb
MaximIvanov <mailto:iv-maxim@ya.ru>
2019-04-01 20:50:14 +0300

    Создать меню. [#4748] ref Исправление замечаний ментора.
    Создать меню. [#4748] ref
    Исправление замечаний ментора.

--- module_02/ood/src/test/java/ru/job4j/ood/menu/MenuTest.java
+++ module_02/ood/src/test/java/ru/job4j/ood/menu/MenuTest.java
@@ -7,10 +7,44 @@ import static org.junit.Assert.*;
 public class MenuTest {
 
     @Test
-    public void menuTest() {
-        Menu menu = new Menu(".");
+    public void whenNewMenuThenItEmpty() {
+        Menu menu = new Menu();
         assertEquals(menu.printMenu(), "");
+    }
 
+    @Test
+    public void whenAddItemThenMenuHasIt() {
+        Menu menu = createMenu();
+        String ls = System.lineSeparator();
+        String result = "- 1 first" + ls
+                + "-- 1.a first A" + ls
+                + "-- 1.b first B" + ls
+                + "--- 1.b.AA first B AA" + ls
+                + "-- 1.c first C" + ls
+                + "- 2 second" + ls;
+        assertEquals(menu.printMenu(), result);
+    }
+
+    @Test
+    public void whenActionNotDefinedThenGetItsMessage() {
+        Menu menu = createMenu();
+        assertEquals(menu.perform("1.b"), "Action not defined. Action \"first B\" not defined.");
+    }
+
+    @Test
+    public void whenWrongKeyThenGetItsMessage() {
+        Menu menu = createMenu();
+        assertEquals(menu.perform("3"), "Wrong key. Key \"3\" not supported.");
+    }
+
+    @Test
+    public void whenActionExistThenPerformItAmdGetOkMessage() {
+        Menu menu = createMenu();
+        assertEquals(menu.perform("1.b.AA"), "ok");
+    }
+
+    private Menu createMenu() {
+        Menu menu = new Menu();
         MenuItem first = new MenuItem("1", "first");
         MenuItem firstA = new MenuItem("a", "first A");
         MenuItem firstB = new MenuItem("b", "first B");
@@ -23,25 +57,12 @@ public class MenuTest {
         menu.addItem(first);
         MenuItem second = new MenuItem("2", "second");
         menu.addItem(second);
-
-        String ls = System.lineSeparator();
-        StringBuilder builder = new StringBuilder();
-        builder.append("- 1 first").append(ls)
-                .append("-- 1.a first A").append(ls)
-                .append("-- 1.b first B").append(ls)
-                .append("--- 1.b.AA first B AA").append(ls)
-                .append("-- 1.c first C").append(ls)
-                .append("- 2 second").append(ls);
-
-        assertEquals(menu.printMenu(), builder.toString());
-        assertEquals(menu.perform("1.b.AA"), "Action not defined. Action \"first B AA\" not defined.");
         firstBAA.setAction(new MenuAction() {
             @Override
             public void perform() {
                 System.out.println("Some action");
             }
         });
-        assertEquals(menu.perform("1.b.AA"), "ok");
-        assertEquals(menu.perform("3"), "Wrong key. Key \"3\" not supported.");
+        return menu;
     }
 }
