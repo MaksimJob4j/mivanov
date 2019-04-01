45c6af4455e46153c712f88ee4b56b3090f3ff8d
MaximIvanov <mailto:iv-maxim@ya.ru>
2019-04-01 20:50:14 +0300

    Создать меню. [#4748] ref Исправление замечаний ментора.

--- module_02/ood/src/main/java/ru/job4j/ood/menu/Menu.java
+++ module_02/ood/src/main/java/ru/job4j/ood/menu/Menu.java
@@ -3,30 +3,21 @@ package ru.job4j.ood.menu;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Menu {
     private final static Logger LOGGER = LogManager.getLogger(Menu.class);
-    private final MenuItem mainMenuItem = new MenuItem("main", "mainMenu");
-    private final String keySeparator;
-
-    public Menu(String keySeparator) {
-        this.keySeparator = keySeparator;
-    }
+    private final MenuItem root = new MenuItem("main", "mainMenu");
 
     public void addItem(MenuItem menuItem) {
         LOGGER.traceEntry();
-        this.mainMenuItem.addSubItem(menuItem);
+        this.root.addSubItem(menuItem);
     }
 
     public String perform(String keyString) {
         LOGGER.traceEntry();
         String result = "ok";
-        List<String> keys = Arrays.stream(keyString.split("[" + keySeparator + "]")).collect(Collectors.toList());
         MenuItem menuItem = null;
         try {
-            menuItem = this.mainMenuItem.getItem(keys);
+            menuItem = this.root.getItem(keyString);
             menuItem.perform();
         } catch (KeyMenuException e) {
             result = String.format("%s Key \"%s\" not supported.", e.getMessage(), keyString);
@@ -40,6 +31,6 @@ public class Menu {
 
     public String printMenu() {
         LOGGER.traceEntry();
-        return this.mainMenuItem.printMenu(this.keySeparator);
+        return this.root.printMenu();
     }
 }
